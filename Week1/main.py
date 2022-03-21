edb893ff2b5a21bdbffc274aa24d4e7d753361f5
ilovejethro <ilovejethro@users.noreply.github.com>
2022-03-20 21:11:28 -0400

    Add files via upload

---
+++ Week1/main.py
@@ -0,0 +1,5 @@
+
+temperature = input('Please enter a temperature in Celsius with one decimal point: ');
+temperature=float(temperature)
+tempFahrenheit = ((9/5)*temperature) + 32;
+print("The temperature in Fahrenheit is: " + str(tempFahrenheit));
